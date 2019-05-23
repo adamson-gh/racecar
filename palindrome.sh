2156d163085e4a18ab4035174605902e63545c77
nosmada <nosmada@users.noreply.github.com>
2019-05-23 10:09:32 -0400

    Uploading palindrome.sh script to repo

---
+++ palindrome.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+until [[ $input = x ]]
+do
+
+echo "Palindrome Checker"
+echo -n "Enter word: " 
+read word
+
+word=$(echo "$word" | tr '[:upper:]' '[:lower:]')
+
+pos=${#word}
+backword=""
+while [ $pos -gt 0 ]
+do
+  clear
+  ((pos--))
+  backword=$backword${word:$pos:1}
+  echo "$word"
+  echo "$backword"
+  sleep 1
+done
+
+echo -n "Result: "
+[[ $word = $backword ]] && echo "True" || echo "False"
+
+echo "(n)ew word or e(x)it"
+read input
+done
+
+echo "Exiting Palindrome Checker ... "
+exit
